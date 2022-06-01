e74844fe4097544a6e5b869e797d447aae59402c
EHSAN <fooehan3@gmail.com>
2022-06-02 00:34:15 +0430

    updeta tipp js

--- University/urls.py
+++ University/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("form_app.urls")),
+    path("form2/", include("form_app.urls")),
     path("form/", include("login_app.urls"))
 
 ]
